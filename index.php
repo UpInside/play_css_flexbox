5da22c292efaee3c84dffce6b765bbc64449b95e
Robson V. Leite <cursos@upinside.com.br>
2017-11-24 16:42:21 -0200

    Exemplos da aula

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <title>Trabalhando de forma otimizada com display flex | ITEM 1</title>
+        <link rel="stylesheet" href="style.css"/>
+    </head>
+    <body>
+        <section class="flex">
+            <article class="flex-item">
+                <h1>Trabalhando de forma otimizada com display flex | ITEM 1</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software.</p>
+            </article>
+
+            <article class="flex-item">
+                <h1>Trabalhando de forma otimizada com display flex | ITEM 2</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            </article>
+
+            <article class="flex-item">
+                <h1>Trabalhando de forma otimizada com display flex | ITEM 3</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled.</p>
+            </article>
+
+            <article class="flex-item">
+                <h1>Trabalhando de forma otimizada com display flex | ITEM 4</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+            </article>
+        </section>
+    </body>
+</html>
